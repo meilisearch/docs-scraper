f869357b02ee9fbacf1891193192654c33f2d856
AABur <aabur@mail.ru>
2021-04-02 18:16:15 +0300

    : Parametrize strip_chars_test
    Parametrize strip_chars_test to avoid duplicate code

--- scraper/src/tests/default_strategy/strip_chars_test.py
+++ scraper/src/tests/default_strategy/strip_chars_test.py
@@ -1,28 +1,36 @@
 # coding: utf-8
 import lxml.html
+import pytest
+
 from .abstract import get_strategy
 
 
 class TestGetRecordsFromDomWithStripChars:
-    def test_strip_chars(self):
+
+    @pytest.mark.parametrize('selectors_lvl1, actual_lvl1',
+                             [
+                                 ('h2', '!Bar'),
+                                 ({'selector': 'h2', 'strip_chars': '!'}, 'Bar.'),
+                             ])
+    def test_strip_chars(self, selectors_lvl1, actual_lvl1):
         # Given
         strategy = get_strategy({
             'selectors': {
-                "lvl0": "h1",
-                "lvl1": "h2",
-                "lvl2": "h3",
-                "content": "p"
+                'lvl0': 'h1',
+                'lvl1': selectors_lvl1,
+                'lvl2': 'h3',
+                'content': 'p'
             },
             'strip_chars': ',.'
         })
-        strategy.dom = lxml.html.fromstring("""
+        strategy.dom = lxml.html.fromstring('''
         <html><body>
             <h1>.Foo;</h1>
             <h2>!Bar.</h2>
             <h3>,Baz!</h3>
             <p>,text,</p>
         </body></html>
-        """)
+        ''')
 
         # When
         actual = strategy.get_records_from_dom()
@@ -31,40 +39,6 @@ class TestGetRecordsFromDomWithStripChars:
         assert len(actual) == 4
         assert actual[3]['type'] == 'content'
         assert actual[3]['hierarchy']['lvl0'] == 'Foo;'
-        assert actual[3]['hierarchy']['lvl1'] == '!Bar'
-        assert actual[3]['hierarchy']['lvl2'] == 'Baz!'
-        assert actual[3]['content'] == 'text'
-
-    def test_strip_chars_with_level_override(self):
-        # Given
-        strategy = get_strategy({
-            'strip_chars': ',.',
-            'selectors': {
-                "lvl0": "h1",
-                "lvl1": {
-                    "selector": "h2",
-                    "strip_chars": "!"
-                },
-                "lvl2": "h3",
-                "content": "p"
-            }
-        })
-        strategy.dom = lxml.html.fromstring("""
-        <html><body>
-            <h1>.Foo;</h1>
-            <h2>!Bar.</h2>
-            <h3>,Baz!</h3>
-            <p>,text,</p>
-        </body></html>
-        """)
-
-        # When
-        actual = strategy.get_records_from_dom()
-
-        # Then
-        assert len(actual) == 4
-        assert actual[3]['type'] == 'content'
-        assert actual[3]['hierarchy']['lvl0'] == 'Foo;'
-        assert actual[3]['hierarchy']['lvl1'] == 'Bar.'
+        assert actual[3]['hierarchy']['lvl1'] == actual_lvl1
         assert actual[3]['hierarchy']['lvl2'] == 'Baz!'
         assert actual[3]['content'] == 'text'
