26838bb15d752d89c9e28c1c60055dafbcfa4808
AABur <aabur@mail.ru>
2021-04-02 17:47:39 +0300

    : Parametrize tags_test
    Parametrize tags_test  to avoid duplicate code

--- scraper/src/tests/default_strategy/tags_test.py
+++ scraper/src/tests/default_strategy/tags_test.py
@@ -1,89 +1,44 @@
 # coding: utf-8
 import lxml.html
+import pytest
+
 from .abstract import get_strategy
 
 
 class TestTags:
-    def test_adding_tags_for_page(self):
+
+    @pytest.mark.parametrize('start_url, actual_url',
+                             [
+                                 # test_adding_tags_for_page
+                                 ('http://foo.bar/api', 'http://foo.bar/api'),
+                                 # test_adding_tags_for_subpage
+                                 ('http://foo.bar/api', 'http://foo.bar/api/test'),
+                                 # test_regex_start_urls
+                                 ('http://foo.bar/*', 'http://foo.bar/api/test'),
+                             ])
+    def test_tags(self, start_url, actual_url):
         # Given
         strategy = get_strategy({
             'start_urls': [{
-                'url': 'http://foo.bar/api',
-                'tags': ["test"]
+                'url': start_url,
+                'tags': ['test']
             }],
             'selectors': {
-                "lvl0": "h1",
-                "lvl1": "h2",
-                "lvl2": "h3",
-                "content": "p"
+                'lvl0': 'h1',
+                'lvl1': 'h2',
+                'lvl2': 'h3',
+                'content': 'p'
             }
         })
 
-        strategy.dom = lxml.html.fromstring("""
+        strategy.dom = lxml.html.fromstring('''
         <html><body>
          <h1>Foo</h1>
         </body></html>
-        """)
+        ''')
 
         # When
-        actual = strategy.get_records_from_dom("http://foo.bar/api")
+        actual = strategy.get_records_from_dom(actual_url)
 
         # Then
-        assert actual[0]['tags'] == ["test"]
-
-    def test_adding_tags_for_subpage(self):
-        # Given
-        strategy = get_strategy({
-            'selectors': {
-                "lvl0": "h1",
-                "lvl1": "h2",
-                "lvl2": "h3",
-                "content": "p"
-            },
-            'start_urls': [{
-                'url': 'http://foo.bar/api',
-                'tags': ["test"]
-            }]
-        })
-
-        strategy.dom = lxml.html.fromstring("""
-        <html><body>
-         <h1>Foo</h1>
-        </body></html>
-        """)
-
-        # When
-        actual = strategy.get_records_from_dom("http://foo.bar/api/test")
-
-        # Then
-        assert actual[0]['tags'] == ["test"]
-
-    def test_regex_start_urls(self):
-        # Given
-        # Stub ENV variables read by ConfigLoader
-        strategy = get_strategy({
-            'start_urls': [
-                {
-                    'url': 'http://foo.bar/.*',
-                    'tags': ["test"]
-                }
-            ],
-            'selectors': {
-                "lvl0": "h1",
-                "lvl1": "h2",
-                "lvl2": "h3",
-                "content": "p"
-            }
-        })
-
-        strategy.dom = lxml.html.fromstring("""
-        <html><body>
-         <h1>Foo</h1>
-        </body></html>
-        """)
-
-        # When
-        actual = strategy.get_records_from_dom("http://foo.bar/api/test")
-
-        # Then
-        assert actual[0]['tags'] == ["test"]
+        assert actual[0]['tags'] == ['test']
