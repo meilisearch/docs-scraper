a9bb8c7a55d7d191544e02a9acfc192921ca22f6
AABur <aabur@mail.ru>
2021-04-02 17:29:51 +0300

    : Parametrize page_rank_test
    Parametrize page_rank_test  to avoid duplicate code

--- scraper/src/tests/default_strategy/page_rank_test.py
+++ scraper/src/tests/default_strategy/page_rank_test.py
@@ -1,19 +1,47 @@
 # coding: utf-8
 import lxml.html
+import pytest
+
 from .abstract import get_strategy
 
 
 class TestPageRank:
-    def test_default_page_rank_should_be_zero(self):
+
+    @pytest.mark.parametrize('page_rank, page_url',
+                             [
+                                 # test_default_page_rank_should_be_zero
+                                 (0, ''),
+                                 # test_positive_page_rank
+                                 (1, 'http://foo.bar/api'),
+                                 # test_positive_sub_page_page_rank
+                                 (1, 'http://foo.bar/api/test'),
+                                 # test_negative_page_rank
+                                 (-1, 'http://foo.bar/api/test'),
+                             ])
+    def test_page_rank(self, page_rank, page_url):
         # Given
-        strategy = get_strategy({
-            'selectors': {
-                "lvl0": "h1",
-                "lvl1": "h2",
-                "lvl2": "h3",
-                "content": "p"
-            }
-        })
+        if page_rank == 0:
+            strategy = get_strategy({
+                'selectors': {
+                    "lvl0": "h1",
+                    "lvl1": "h2",
+                    "lvl2": "h3",
+                    "content": "p"
+                }
+            })
+        else:
+            strategy = get_strategy({
+                'start_urls': [{
+                    'url': 'http://foo.bar/api',
+                    'page_rank': page_rank
+                }],
+                'selectors': {
+                    "lvl0": "h1",
+                    "lvl1": "h2",
+                    "lvl2": "h3",
+                    "content": "p"
+                }
+            })
 
         strategy.dom = lxml.html.fromstring("""
         <html><body>
@@ -22,88 +50,7 @@ class TestPageRank:
         """)
 
         # When
-        actual = strategy.get_records_from_dom()
+        actual = strategy.get_records_from_dom(page_url)
 
         # Then
-        assert actual[0]['weight']['page_rank'] == 0
-
-    def test_positive_page_rank(self):
-        # Given
-        strategy = get_strategy({
-            'start_urls': [{
-                'url': 'http://foo.bar/api',
-                'page_rank': 1
-            }],
-            'selectors': {
-                "lvl0": "h1",
-                "lvl1": "h2",
-                "lvl2": "h3",
-                "content": "p"
-            }
-        })
-
-        strategy.dom = lxml.html.fromstring("""
-        <html><body>
-         <h1>Foo</h1>
-        </body></html>
-        """)
-
-        # When
-        actual = strategy.get_records_from_dom("http://foo.bar/api")
-
-        # Then
-        assert actual[0]['weight']['page_rank'] == 1
-
-    def test_positive_sub_page_page_rank(self):
-        # Given
-        strategy = get_strategy({
-            'start_urls': [{
-                'url': 'http://foo.bar/api',
-                'page_rank': 1
-            }],
-            'selectors': {
-                "lvl0": "h1",
-                "lvl1": "h2",
-                "lvl2": "h3",
-                "content": "p"
-            }
-        })
-
-        strategy.dom = lxml.html.fromstring("""
-        <html><body>
-         <h1>Foo</h1>
-        </body></html>
-        """)
-
-        # When
-        actual = strategy.get_records_from_dom("http://foo.bar/api/test")
-
-        # Then
-        assert actual[0]['weight']['page_rank'] == 1
-
-    def test_negative_page_rank(self):
-        # Given
-        strategy = get_strategy({
-            'start_urls': [{
-                'url': 'http://foo.bar/api',
-                'page_rank': -1
-            }],
-            'selectors': {
-                "lvl0": "h1",
-                "lvl1": "h2",
-                "lvl2": "h3",
-                "content": "p"
-            }
-        })
-
-        strategy.dom = lxml.html.fromstring("""
-        <html><body>
-         <h1>Foo</h1>
-        </body></html>
-        """)
-
-        # When
-        actual = strategy.get_records_from_dom("http://foo.bar/api/test")
-
-        # Then
-        assert actual[0]['weight']['page_rank'] == -1
+        assert actual[0]['weight']['page_rank'] == page_rank
