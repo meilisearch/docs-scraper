dcfe02c2b8c56352b12e1f3e05243e52cd4033a4
Revathy Venugopal <104772255+Revathyvenugopal162@users.noreply.github.com>
2023-07-26 08:18:11 -0400

    CI: add the ci_cd (#8)
    Co-authored-by: Jorge Martínez <28702884+jorgepiloto@users.noreply.github.com>

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -1,1 +1,11 @@
 
+"""PyMeilisearch scrapper"""
+
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:  # pragma: no cover
+    import importlib_metadata
+
+# Read from the pyproject.toml
+# major, minor, patch
+__version__ = importlib_metadata.version("pymeilisearch-scraper")
